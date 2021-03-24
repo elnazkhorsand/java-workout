2cbeeac8611fb24149abb97971ea5f3114654f9a
Mina(Elnaz) Khorsand Mahand <elnazkhorsand@gmail.com>
2021-03-24 14:29:00 +0100

    Fixing 'shouldReportNonSupportedOperations' Test.

--- src/main/java/com/serenitydojo/calculator/Calculator.java
+++ src/main/java/com/serenitydojo/calculator/Calculator.java
@@ -2,12 +2,34 @@ package com.serenitydojo.calculator;
 
 public class Calculator {
 
+    private String[] elements;
+    private String[] validOperators = new String[]{"+", "-", "*"};
+    private String[] numbers;
+    private boolean isValidElement = false;
+
     public int evaluate(String expression) {
+
+        //Checking if the expression is empty return 0.
         if(expression.isBlank())
             return 0;
-        return -1;
+
+        //Getting all the elements in the expression and split them by space and keep them in 'elements' array.
+        elements = expression.split(" ");
+
+        for(String element: elements){
+            if (!checkValidity(element))
+                throw new IllegalMathsOperatorException("There is an invalid element in the expression");
+        }
+
+    return -1;
     }
 
-
+    private boolean checkValidity(String element){
+        for (String operator: validOperators){
+            if(element.equals(operator))
+                return true;
+        }
+        return element.chars().allMatch(Character::isDigit);
+    }
 
 }
