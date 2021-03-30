8e7beab28158f985fc35fafb4a41c4e2ba506240
Mina(Elnaz) Khorsand Mahand <elnazkhorsand@gmail.com>
2021-03-30 14:08:48 +0200

    Added 'reverseAListAndReturnIt' Test.

--- src/main/java/com/serenitydojo/strings/MyUtility.java
+++ src/main/java/com/serenitydojo/strings/MyUtility.java
@@ -1,5 +1,8 @@
 package com.serenitydojo.strings;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyUtility {
     public static String reverse(String firstString, String secondString) {
         return secondString+" "+firstString;
@@ -19,5 +22,16 @@ public class MyUtility {
         }
 
         return result.toString().trim();
+    }
+
+    public static List<String> reverseList(List<String> originalList) {
+        int lastIndexOfOriginalList = originalList.size()-1;
+        List<String> reversedList = new ArrayList<>();
+
+        for (int i = lastIndexOfOriginalList;i>=0;i--){
+            reversedList.add(originalList.get(i));
+        }
+
+        return reversedList;
     }
 }
--- src/test/java/com/serenitydojo/strings/WhenWorkingWithJavaStrings.java
+++ src/test/java/com/serenitydojo/strings/WhenWorkingWithJavaStrings.java
@@ -1,6 +1,10 @@
 package com.serenitydojo.strings;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class WhenWorkingWithJavaStrings {
@@ -20,6 +24,14 @@ public class WhenWorkingWithJavaStrings {
     @Test
     public void reverseArrayOfStrings(){
         assertThat(MyUtility.reverse("One", "Two", "Three", "Four", "Five", "Six")).isEqualTo("Six Five Four Three Two One");
+    }
+
+    @Test
+    public void reverseAListAndReturnIt(){
+        List<String> originalList = Arrays.asList("One", "Two", "Three", "Four");
+        List<String> expectedReversed = Arrays.asList("Four", "Three", "Two", "One");
+
+        assertThat(MyUtility.reverseList(originalList)).isEqualTo(expectedReversed);
     }
 
 }
